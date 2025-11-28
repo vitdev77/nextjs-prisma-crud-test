33024e6f1f985ceed2954f38697abbcdcf9dde65
vitdev77 <vit.dev.77@gmail.com>
2025-11-28 23:53:28 +0300

    28112025_008_commit

--- components/view-post-component.tsx
+++ components/view-post-component.tsx
@@ -12,9 +12,9 @@ export function ViewPostComponent({ post, className }: Props) {
       <h2 className="text-2xl font-bold">{post.title}</h2>
       <p className="text-muted-foreground">by {post.author.name}</p>
       <div className="p-4 border border-dashed rounded-lg">{post.content}</div>
-      <pre className="whitespace-pre font-mono text-xs text-muted-foreground bg-muted p-4 rounded-lg w-full">
+      {/* <pre className="whitespace-pre font-mono text-xs text-muted-foreground bg-muted p-4 rounded-lg w-full">
         {JSON.stringify(post, null, 2)}
-      </pre>
+      </pre> */}
     </div>
   );
 }
