9474a6fdfa5b2276565d250df3fd7bfa3bf014f3
vitdev77 <vitdev77@gmail.com>
2025-11-28 15:56:23 +0300

    28112025_004_commit

--- components/edit-post-component.tsx
+++ components/edit-post-component.tsx
@@ -9,7 +9,13 @@ interface Props {
 export function EditPostComponent({ post, className }: Props) {
   return (
     <div className={cn("space-y-2", className)}>
-      EditPostComponent: #{post.id}
+      <div className="text-destructive text-lg font-medium">
+        EditPostComponent: #{post.id}
+      </div>
+      <pre className="whitespace-pre font-mono text-xs text-muted-foreground bg-muted p-4 rounded-lg w-full">
+        {JSON.stringify(post, null, 2)}
+      </pre>
+      {/* Edit Post Form Here */}
     </div>
   );
 }
--- components/forms/edit-post-form.tsx
+++ components/forms/edit-post-form.tsx
@@ -1,17 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import {
   Form,
   FormControl,
@@ -21,104 +10,156 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Pen } from "lucide-react";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { cn } from "@/lib/utils";
-import { toast } from "sonner";
 import { LoadingButton } from "@/components/loading-button";
-import { editPost } from "@/actions/post.actions";
+import { toast } from "sonner";
+import { createPost } from "@/actions/post.actions";
 
 const editPostSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   content: z.string().min(1, { message: "Content is required" }),
+  authorId: z.string().nonempty("Please select an author"),
 });
 
 type EditPostValues = z.infer<typeof editPostSchema>;
 
-export function EditPostForm({ id }: { id: string }) {
-  const [open, setOpen] = React.useState(false);
+export function EditPostForm({
+  _onSubmit,
+  // users,
+}: {
+  _onSubmit?: VoidFunction;
+  // users: Promise<{ id: number; email: string; name: string | null }[]>;
+}) {
   const [error, setError] = React.useState<string | null>(null);
+
+  // const allUsers = React.use(users);
 
   const form = useForm<EditPostValues>({
     resolver: zodResolver(editPostSchema),
     defaultValues: {
       title: "",
       content: "",
+      authorId: "",
     },
   });
 
   const onSubmit = form.handleSubmit(async (data) => {
-    toast.success(JSON.stringify(data));
-    setOpen(false);
+    const res = await createPost(data);
+    if (res?.error) {
+      toast.error(res.error);
+    } else {
+      toast.success("Post data successfully updated");
+      _onSubmit?.();
+    }
   });
 
   const loading = form.formState.isSubmitting;
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button size={"icon-sm"} variant={"ghost"}>
-          <Pen />
-          <span className="sr-only">Edit</span>
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit post</DialogTitle>
-          <DialogDescription>
-            All form fields are required. Click button below when you&apos;re
-            done.
-          </DialogDescription>
-        </DialogHeader>
+    <div className="flex flex-col gap-6">
+      <div className="space-y-2">
+        <h2 className="text-lg md:text-xl leading-none font-semibold">
+          Edit post
+        </h2>
+        <p className="text-xs md:text-sm text-muted-foreground">
+          Fill all form fields. Click button below when you&apos;re done.
+        </p>
+      </div>
 
-        <Form {...form}>
-          <form onSubmit={onSubmit} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Title" disabled={loading} {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="content"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Content</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="Content here..."
-                      disabled={loading}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            {error && (
-              <div role="alert" className="text-destructive text-sm">
-                {error}
-              </div>
+      <Form {...form}>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" disabled={loading} {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
             )}
+          />
 
-            <LoadingButton type="submit" className="w-full" loading={loading}>
-              Submit
-            </LoadingButton>
-          </form>
-        </Form>
-      </DialogContent>
-    </Dialog>
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Content</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Content here..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="authorId"
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Author</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    // disabled={loading || allUsers.length === 0}
+                    disabled={loading}
+                    {...field}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select an author" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Authors</SelectLabel>
+                        {/* {allUsers.map((user) => (
+                          <SelectItem key={user.id} value={String(user.id)}>
+                            {user.name} #{user.email}
+                          </SelectItem>
+                        ))} */}
+                        <SelectItem value="1">Alice</SelectItem>
+                        <SelectItem value="2">Bob</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+
+          {error && (
+            <div role="alert" className="text-destructive text-sm">
+              {error}
+            </div>
+          )}
+
+          <LoadingButton type="submit" className="w-full" loading={loading}>
+            Submit
+          </LoadingButton>
+        </form>
+      </Form>
+    </div>
   );
 }
